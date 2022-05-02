d3db3b44c7ec9328161fa51e67b0e77d01a70688
Heather Shultz <silverlycold@gmail.com>
2022-05-01 22:55:39 -0500

    Create private&protected.py

---
+++ private&protected.py
@@ -0,0 +1,35 @@
+#
+# Python:    3.10
+#
+# Author:    H.Shultz
+#
+# Purpose:   TTA - Python Course, Created a class that uses encapsulation by
+#            making use of a private attribute and a protected attribute, then
+#            created an object that uses both.
+
+
+
+#creating class
+class one:
+    #creating instance
+    def __init__(self):
+        # Protected varabile
+        self._protected = 2
+        # Private varabile
+        self.__private = 5
+
+    def getPrivate(self):
+        print(self.__private)
+    
+    def getProtected(self):
+        print(self._protected)
+
+test = one()
+#call to private varabile
+print(test.getPrivate)
+#call to protected varabile
+print(test.getProtected)
+
+
+
+
